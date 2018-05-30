cb4fdee4bc13367336aef86d4ce10cc9b4c1eb4d
Yash Agrawal <yagrawal900@gmail.com>
2018-05-30 19:21:29 +0530

    removed MANIFEST

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
 #!/usr/bin/env python
-try:
-   from sugar3.activity import bundlebuilder
-   bundlebuilder.start()
-except ImportError:
-   import os
-   os.system("find ./ | grep -v .svn | grep -v '.pyc$' | sed 's,^./,falabracman.activity/,g' > MANIFEST")
-   os.chdir('..')
-   os.system('rm falabracman.xo')
-   os.system('cat falabracman.activity/MANIFEST | zip falabracman.xo -@')
-   #os.system('mv falabracman.xo ./falabracman.activity')
-   os.chdir('falabracman.activity')
+from sugar3.activity import bundlebuilder
+bundlebuilder.start()
